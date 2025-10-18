5f7e1bb4d20239f389970669ddd0cbfe58fc4be7
Yustar <36198000+yuzutaru@users.noreply.github.com>
2025-10-18 09:04:04 +0700

    Add(Theme, Color): prepare color for light and dark theme

--- app/src/main/java/com/yustar/wellness/ui/theme/Color.kt
+++ app/src/main/java/com/yustar/wellness/ui/theme/Color.kt
@@ -9,3 +9,18 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val White = Color(0xFFFFFFFF)
+
+val Gray10 = Color(0xFFF2F5F9)
+val Gray20 = Color(0xFFDCE1E8)
+val Gray60 = Color(0xFF5D6A85)
+val Gray80 = Color(0xFF242E49)
+val Gray90 = Color(0xFF141B31)
+val Gray100 = Color(0xFF090E1D)
+
+val Blue10 = Color(0xFFEDF5FF)
+val Blue20 = Color(0xFFD0E4FF)
+val Blue60 = Color(0xFF0F67FE)
+val Blue80 = Color(0xFF00349C)
--- app/src/main/java/com/yustar/wellness/ui/theme/Theme.kt
+++ app/src/main/java/com/yustar/wellness/ui/theme/Theme.kt
@@ -12,25 +12,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    background = Gray100,
+    surface = Gray90,
+    onPrimaryFixedVariant = Blue60,
+    onPrimary = Blue80,
+    onPrimaryContainer = Blue10,
+    onSecondaryFixedVariant = Gray80,
+    onSecondary = White,
+    onTertiaryFixed = Gray60,
+    onTertiary = Gray20
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    /* Other default colors to override*/
+    background = Gray10,
+    surface = White,
+    onPrimaryFixedVariant = Blue60,
+    onPrimary = Blue60,
+    onPrimaryContainer = Blue10,
+    onSecondaryFixedVariant = Gray80,
+    onSecondary = Gray80,
+    onTertiaryFixed = Gray60,
+    onTertiary = Gray60
 )
 
 @Composable
